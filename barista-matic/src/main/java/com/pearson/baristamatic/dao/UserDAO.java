cd6585b704167b85e0aa29fcc6e8a7805cc0c0dd
Matthew Serrano <matteserrano@gmail.com>
2013-07-15 13:43:03 -0400

    Added public modifier to "findUsers(String userName)" method.

--- barista-matic/src/main/java/com/pearson/baristamatic/dao/UserDAO.java
+++ barista-matic/src/main/java/com/pearson/baristamatic/dao/UserDAO.java
@@ -6,5 +6,5 @@ import com.pearson.baristamatic.entity.User;
 
 public interface UserDAO extends GenericDAO<User, String> {
 	public void saveUser(User user);
-	List<User> findUsers(String userName);
+	public List<User> findUsers(String userName);
 }
