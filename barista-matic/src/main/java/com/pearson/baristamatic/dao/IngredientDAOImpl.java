386f941041bbe459cc7b80a063499dc35ec5c961
Matthew Serrano <matteserrano@gmail.com>
2013-07-17 15:26:43 -0400

    Added ingredient DAO and Service interface and class files.

---
+++ barista-matic/src/main/java/com/pearson/baristamatic/dao/IngredientDAOImpl.java
@@ -0,0 +1,38 @@
+package com.pearson.baristamatic.dao;
+
+import java.util.List;
+
+import org.hibernate.criterion.Restrictions;
+
+import com.pearson.baristamatic.entity.Ingredient;
+
+public class IngredientDAOImpl extends GenericDAOImpl<Ingredient, String> implements IngredientDAO {
+
+	@Override
+	public Ingredient findIngredient(String ingredientName) {
+		List<Ingredient> ingredients = findByCriteria(Restrictions.like("ingredientName", ingredientName));
+		
+		if (ingredients.size() != 1)
+			return null;
+		
+		return ingredients.get(0);
+	}
+
+	@Override
+	@SuppressWarnings("unchecked")
+	public List<Ingredient> findIngredients() {
+		return getCurrentSession().createQuery("from Ingredient").list();
+	}
+
+	@Override
+	public void saveOrUpdateIngredient(Ingredient ingredient) {
+		saveOrUpdate(ingredient);
+	}
+
+	@Override
+	public void deleteIngredient(String ingredientName) {
+		Ingredient ingredient = findIngredient(ingredientName);
+		if (ingredient != null)
+			delete(ingredient);
+	}
+}
