8adf569beeb9e754f903b705ed0b8c9568c02435
hemanthsj <hemanth_janakiram@yahoo.com>
2013-07-18 14:15:26 -0400

    Created some sort of spring security framework

--- barista-matic/src/main/java/com/pearson/baristamatic/service/CustomUserDetailsService.java
+++ barista-matic/src/main/java/com/pearson/baristamatic/service/CustomUserDetailsService.java
@@ -1,14 +1,22 @@
 package com.pearson.baristamatic.service;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.pearson.baristamatic.dao.UserDAO;
-import com.pearson.baristamatic.entity.User;
+//import com.pearson.baristamatic.entity.User;
+import com.pearson.baristamatic.entity.User.Role;
 
 @Service
 @Transactional(readOnly=true)
@@ -20,7 +28,7 @@ public class CustomUserDetailsService implements UserDetailsService {
 	public UserDetails loadUserByUsername(String login)
 			throws UsernameNotFoundException {
 		
-		User baristaUser = userDAO.findUser(login);
+		com.pearson.baristamatic.entity.User baristaUser = userDAO.findUser(login);
 		
 		 /* boolean enabled = true;
 		  boolean accountNonExpired = true;
@@ -28,18 +36,36 @@ public class CustomUserDetailsService implements UserDetailsService {
 		  boolean accountNonLocked = true;*/
 		 
 		
-		return (UserDetails) new User(baristaUser.getUserName(),
+		return new User(baristaUser.getUserName(),
 				        baristaUser.getPassword(),
-				        baristaUser.getRole());
+				        getAuthorities(baristaUser.getRole()));
 		
 	}
+
+	private Collection<? extends GrantedAuthority> getAuthorities(Role role) {
+		List<GrantedAuthority> authList = getGrantedAuthorities(getRoles(role));
+		return authList;
+	}
 	
-/*	private String getRole(Role role){
+	private List<String> getRoles(Role role){
 		
-		if(role.equals("CUSTOMER"))
-			return "CUSTOMER";
+		List<String> roles = new ArrayList<String>();
+
+		if(role.equals(Role.CUSTOMER))
+			roles.add("CUSTOMER");
 		else
-			return "ADMINISTRATOR";
-	}*/
+			roles.add("ADMINISTRATOR");
+		return roles;
+	}
+
+		
+	private List<GrantedAuthority> getGrantedAuthorities(List<String> roles) {
+		List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
+
+		for (String role : roles) {
+			authorities.add(new SimpleGrantedAuthority(role));
+		}
+		return authorities;
+	}
 
 }
