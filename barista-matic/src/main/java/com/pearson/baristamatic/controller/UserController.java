b70c1e5d0919fefcf71614ddebba7cd7dfa7ef1a
hemanthsj <hemanth_janakiram@yahoo.com>
2013-07-19 00:56:24 -0400

    Implemented redirection but implementing the Authentication Success Handler.

--- barista-matic/src/main/java/com/pearson/baristamatic/controller/UserController.java
+++ barista-matic/src/main/java/com/pearson/baristamatic/controller/UserController.java
@@ -1,24 +1,91 @@
 package com.pearson.baristamatic.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import java.io.IOException;
+import java.util.Collection;
 
-import com.pearson.baristamatic.service.UserService;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
+import org.springframework.security.web.WebAttributes;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Controller;
+
+import com.pearson.baristamatic.entity.User.Role;
+
 
 @Controller
-public class UserController {
+public class UserController implements AuthenticationSuccessHandler {
 	
-	@Autowired
-	private UserService userService;
+	protected Log logger = LogFactory.getLog(this.getClass());
+	 
+    private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+
+	@Override
+	public void onAuthenticationSuccess(HttpServletRequest request,
+			HttpServletResponse response, Authentication authentication)
+			throws IOException, ServletException {
+		handle(request, response, authentication);
+        clearAuthenticationAttributes(request);
+		
+	}
+
+	private void clearAuthenticationAttributes(HttpServletRequest request) {
+		HttpSession session = request.getSession(false);
+        if (session == null) {
+            return;
+        }
+        session.removeAttribute(WebAttributes.AUTHENTICATION_EXCEPTION);
+		
+	}
+
+	private void handle(HttpServletRequest request,
+			HttpServletResponse response, Authentication authentication) throws IOException {
+		String targetUrl = determineTargetUrl(authentication);
+		 
+        if (response.isCommitted()) {
+            logger.debug("Response has already been committed. Unable to redirect to " + targetUrl);
+            return;
+        }
  
-	@RequestMapping(value="/users", method=RequestMethod.GET)
-	public ResponseEntity<Void> getUsers(ModelMap model) {
+        redirectStrategy.sendRedirect(request, response, targetUrl);
+    }
+
+	private String determineTargetUrl(Authentication authentication) {
+		boolean isUser = false;
+        boolean isAdmin = false;
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        for (GrantedAuthority grantedAuthority : authorities) {
+            if (grantedAuthority.getAuthority().equals(Role.CUSTOMER.toString())) {
+                isUser = true;
+                break;
+            } else if (grantedAuthority.getAuthority().equals(Role.ADMINISTRATOR.toString())) {
+                isAdmin = true;
+                break;
+            }
+        }
  
-		userService.findUsers();
-		return null;
+        if (isUser) {
+            return "/homepage.html";
+        } else if (isAdmin) {
+            return "/console.html";
+        } else {
+            throw new IllegalStateException();
+        }
+	}
+	
+	public void setRedirectStrategy(RedirectStrategy redirectStrategy){
+		this.redirectStrategy = redirectStrategy;
+	}
+	
+	public RedirectStrategy getRedirectStrategy(){
+		return redirectStrategy;
 	}
 }
