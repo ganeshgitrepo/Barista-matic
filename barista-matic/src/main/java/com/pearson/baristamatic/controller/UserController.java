f595ed3a8402df698d189d6784042a275dd5ab4c
Matthew Serrano <matteserrano@gmail.com>
2013-07-21 11:06:05 -0400

    Renamed UserController (which handled authentication) to LoginController. A new UserController class was created to facilitate user account options (registering, deleting).

--- barista-matic/src/main/java/com/pearson/baristamatic/controller/UserController.java
+++ barista-matic/src/main/java/com/pearson/baristamatic/controller/UserController.java
@@ -1,92 +1,32 @@
 package com.pearson.baristamatic.controller;
 
-import java.io.IOException;
-import java.util.Collection;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.web.DefaultRedirectStrategy;
-import org.springframework.security.web.RedirectStrategy;
-import org.springframework.security.web.WebAttributes;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import com.pearson.baristamatic.entity.User;
+import com.pearson.baristamatic.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
-import com.pearson.baristamatic.entity.User.Role;
-
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
-public class UserController implements AuthenticationSuccessHandler {
-	
-	protected Log logger = LogFactory.getLog(this.getClass());
-	 
-    private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+@RequestMapping(value="/user")
+public class UserController {
 
-	@Override
-	public void onAuthenticationSuccess(HttpServletRequest request,
-			HttpServletResponse response, Authentication authentication)
-			throws IOException, ServletException {
-		handle(request, response, authentication);
-        clearAuthenticationAttributes(request);
-		
-	}
+	@Autowired
+    private UserService userService;
 
-	private void clearAuthenticationAttributes(HttpServletRequest request) {
-		HttpSession session = request.getSession(false);
-        if (session == null) {
-            return;
-        }
-        session.removeAttribute(WebAttributes.AUTHENTICATION_EXCEPTION);
-		
-	}
-
-	private void handle(HttpServletRequest request,
-			HttpServletResponse response, Authentication authentication) throws IOException {
-		String targetUrl = determineTargetUrl(authentication);
-		 
-        if (response.isCommitted()) {
-            logger.debug("Response has already been committed. Unable to redirect to " + targetUrl);
-            return;
-        }
- 
-
-        redirectStrategy.sendRedirect(request, response, targetUrl);
+    @ResponseStatus(HttpStatus.CREATED)
+    @RequestMapping(value="", method= RequestMethod.POST)
+    public @ResponseBody Map<String, String> registerUser(User user) {
+        userService.saveOrUpdateUser(user);
+        Map<String, String> map = new HashMap();
+        map.put("Created user", user.getUserName());
+        return map;
     }
 
-	private String determineTargetUrl(Authentication authentication) {
-		boolean isUser = false;
-        boolean isAdmin = false;
-        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
-        for (GrantedAuthority grantedAuthority : authorities) {
-            if (grantedAuthority.getAuthority().equals(Role.CUSTOMER.toString())) {
-                isUser = true;
-                break;
-            } else if (grantedAuthority.getAuthority().equals(Role.ADMINISTRATOR.toString())) {
-                isAdmin = true;
-                break;
-            }
-        }
- 
-        if (isUser) {
-            return "/api/drink";
-        } else if (isAdmin) {
-            return "/api/ingredient";
-        } else {
-            throw new IllegalStateException();
-        }
-	}
-	
-	public void setRedirectStrategy(RedirectStrategy redirectStrategy){
-		this.redirectStrategy = redirectStrategy;
-	}
-	
-	public RedirectStrategy getRedirectStrategy(){
-		return redirectStrategy;
-	}
 }
