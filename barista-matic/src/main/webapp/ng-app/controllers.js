12f86e1991487d41a9ce696ff08a23a16fe155c4
Matthew Serrano <matteserrano@gmail.com>
2013-07-24 10:47:03 -0400

    Re-arranged methods in

--- barista-matic/src/main/webapp/ng-app/controllers.js
+++ barista-matic/src/main/webapp/ng-app/controllers.js
@@ -1,18 +1,47 @@
-app.controller('LoginCtrl', function($scope, $location, userAuthService, userService) {
-	$scope.$watch(function redirectUser() {
-		var role = userService.getUserDetails().role;
-		if (role == "CUSTOMER") {
-			$location.path('/drink').replace();
-		} else if (role == "ADMINISTRATOR") {
-			$location.path('/ingredient').replace();
-		}
+app.controller('LoginCtrl',  function($scope, $location, userAuthService, userService) {
+    $scope.$watch(function redirectUser() {
+        var role = userService.getUserDetails().role;
+        if (role == "CUSTOMER") {
+            $location.path('/drink').replace();
+        } else if (role == "ADMINISTRATOR") {
+            $location.path('/ingredient').replace();
+        }
+    });
+
+    $scope.login = function(user) {
+        if ($scope.user == undefined) {
+            return;
+        }
+        userAuthService.login($scope.user.username, $scope.user.password);
+    };
+});
+
+app.controller('LogoutCtrl', function($scope, $location, userAuthService) {
+    userAuthService.logout();
+    $location.path('/login').replace();
+});
+
+app.controller('DrinkCtrl', function($scope, drinkService) {
+	drinkService.getDrinks(function (response){
+	    console.log(response);
+	    $scope.drinks = response;
 	});
 
-	$scope.login = function(user) {
-		if ($scope.user == undefined) {
-			return;
-		}
-		userAuthService.login($scope.user.username, $scope.user.password);
+	$scope.buyDrink = function(drink){
+		$scope.selection = drink;
+		console.log($scope.selection);
+	};
+});
+
+app.controller('IngredientCtrl', function($scope,ingredientService) {
+	ingredientService.getIngredients(function(response){
+		console.log(response);
+		$scope.ingredients = response;
+	});
+
+	$scope.restockIngredient = function(ingredient, amount, response) {
+		$scope.selection = ingredient;
+		console.log($scope.selection.inventory);
 	};
 });
 
@@ -28,34 +57,4 @@ app.controller('NavCtrl', function($scope, $location, userService) {
 			$scope.isCustomer = false;
 		}
 	});
-});
-
-app.controller('LogoutCtrl', function($scope, $location, userAuthService) {
-	userAuthService.logout();
-	$location.path('/login').replace();
-});
-
-app.controller('DrinkCtrl', function($scope, drinkService) {
-	drinkService.getDrinks(function (response){
-		console.log(response);
-		$scope.drinks = response;
-	});
-
-	$scope.buyDrink = function(drink){
-		$scope.selection = drink;
-		console.log($scope.selection);
-	};
-});
-
-app.controller('IngredientCtrl', function($scope,ingredientService) {
-
-	ingredientService.getIngredients(function(response){
-		console.log(response);
-		$scope.ingredients = response;
-	});
-
-	$scope.restock = function(ingredient, amount, response) {
-		$scope.selection = ingredient;
-		console.log($scope.selection.inventory);
-	};
 });
--- barista-matic/src/main/webapp/ng-app/services.js
+++ barista-matic/src/main/webapp/ng-app/services.js
@@ -1,65 +1,65 @@
 var services = angular.module('services', ['ngResource', 'ngCookies']).
-factory('userService', function($cookieStore, $http) {
-	init();
+    factory('userService', function($cookieStore, $http) {
+        init();
 
-	var service = {
-			auth : '',
-			userId: '',
-			username: '',
-			role: '',
-			isLoggedIn: false
-	};
+        var service = {
+            auth : '',
+            userId: '',
+            username: '',
+            role: '',
+            isLoggedIn: false
+        };
 
-	// if both cookies are stored, retrieve user
-	function init() {
-		var auth = $cookieStore.get('auth');
-		if (auth != null) {
-			console.log("Credentials saved. Retrieving user..");
-			$http.defaults.headers.common['Authorization'] = auth;
-			$http.post('api/user/auth').success(function(data, status, headers, config) {
-				service.userId = data.userId;
-				service.username = data.userName;
-				service.role = data.role;
-				service.isLoggedIn = true;
-			});
-		}
-	}
+        // if both cookies are stored, retrieve user
+        function init() {
+            var auth = $cookieStore.get('auth');
+            if (auth != null) {
+                console.log("Credentials saved. Retrieving user..");
+                $http.defaults.headers.common['Authorization'] = auth;
+                $http.post('api/user/auth').success(function(data, status, headers, config) {
+                    service.userId = data.userId;
+                    service.username = data.userName;
+                    service.role = data.role;
+                    service.isLoggedIn = true;
+                });
+            }
+        }
 
-	service.getUserDetails = function() {
-		return service;
-	}
+        service.getUserDetails = function() {
+            return service;
+        }
 
-	service.setUserDetails = function(userId, username, auth, role) {
-		service.userId = userId;
-		service.username = username;
-		service.auth = auth;
-		$cookieStore.put("auth", auth);
-		service.role = role;
-		service.isLoggedIn = true;
-	}
+        service.setUserDetails = function(userId, username, auth, role) {
+            service.userId = userId;
+            service.username = username;
+            service.auth = auth;
+            $cookieStore.put("auth", auth);
+            service.role = role;
+            service.isLoggedIn = true;
+        }
 
-	service.destroyUserDetails = function() {
-		$cookieStore.remove("auth");
-		service = {};
-	}
+        service.destroyUserDetails = function() {
+            $cookieStore.remove("auth");
+            service = {};
+        }
 
-	return service;
-}).
-service('userAuthService', function($http, userService) {
-	this.login = function(username, password) {
-		var userPass = username + ':' + password;
-		var base64 = window.btoa(unescape(encodeURIComponent(userPass)));
-		var customHeaders = 'Basic ' + base64;
-		$http.defaults.headers.common['Authorization'] = customHeaders;
-		$http.post('api/user/auth').success(function(data, status, headers, config) {
-			console.log("We are authenticated!");
-			userService.setUserDetails(data.userId, username, customHeaders, data.role);
-			return true;
-		}).error(function(data, status, headers, config) {
-			console.log("We could not authenticate successfully.");
-			return false;
-		});
-	};
+        return service;
+    }).
+    service('userAuthService', function($http, userService) {
+        this.login = function(username, password) {
+            var userPass = username + ':' + password;
+            var base64 = window.btoa(unescape(encodeURIComponent(userPass)));
+            var customHeaders = 'Basic ' + base64;
+            $http.defaults.headers.common['Authorization'] = customHeaders;
+            $http.post('api/user/auth').success(function(data, status, headers, config) {
+                console.log("We are authenticated!");
+                userService.setUserDetails(data.userId, username, customHeaders, data.role);
+                return true;
+            }).error(function(data, status, headers, config) {
+                console.log("We could not authenticate successfully.");
+                return false;
+            });
+        };
 
 	this.logout = function() {
 		userService.destroyUserDetails();
