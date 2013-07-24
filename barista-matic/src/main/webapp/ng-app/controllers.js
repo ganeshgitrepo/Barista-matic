de0bb611aeb4f84cd9df0d9cda8d4e0a0fea1bb0
hemanthsj <hemanth_janakiram@yahoo.com>
2013-07-24 09:34:17 -0400

    Merging with main branch

--- barista-matic/src/main/webapp/ng-app/controllers.js
+++ barista-matic/src/main/webapp/ng-app/controllers.js
@@ -1,45 +1,45 @@
-app.controller('NavCtrl', function($scope, $location, userService) {
-	$scope.$watch(function showNav() {
-		$scope.isLoggedIn = userService.getUserDetails().isLoggedIn;
-		$scope.username = userService.getUserDetails().username;
-	});
+app.controller('LoginCtrl', function($scope, $location, userAuthService, userService) {
+    $scope.$watch(function redirectUser() {
+        var role = userService.getUserDetails().role;
+        if (role == "CUSTOMER") {
+            $location.path('/drink').replace();
+        } else if (role == "ADMINISTRATOR") {
+            $location.path('/ingredient').replace();
+        }
+    });
+
+    $scope.login = function(user) {
+        if ($scope.user == undefined) {
+            return;
+        }
+        userAuthService.login($scope.user.username, $scope.user.password);
+    };
 });
 
-app.controller('LoginCtrl', function($scope, $location, userAuthService,
-		userService) {
-	$scope.$watch(function redirectUser() {
-		var role = userService.getUserDetails().role;
-		if (role == "CUSTOMER") {
-			$location.path('/drink').replace();
-		} else if (role == "ADMINISTRATOR") {
-			$location.path('/ingredient').replace();
-		}
-	});
-
-	$scope.login = function(user) {
-		if ($scope.user == undefined) {
-			return;
-		}
-		userAuthService.login($scope.user.username, $scope.user.password);
-	}
+app.controller('NavCtrl', function($scope, $location, userService) {
+    $scope.$watch(function showNav() {
+        $scope.isLoggedIn = userService.getUserDetails().isLoggedIn;
+        $scope.username = userService.getUserDetails().username;
+        if (userService.getUserDetails().role == "CUSTOMER") {
+            $scope.isCustomer = true;
+            $scope.isAdmin = false;
+        } else {
+            $scope.isAdmin = true;
+            $scope.isCustomer = false;
+        }
+    });
 });
 
 app.controller('LogoutCtrl', function($scope, $location, userAuthService) {
-	userAuthService.logout();
-	$location.path('/login').replace();
+    userAuthService.logout();
+    $location.path('/login').replace();
 });
 
-app.controller('DrinkCtrl', function($scope) {
-	$scope.drinks = [ {
-		"name" : "Coffee",
-		"cost" : "$3.85"
-	}, {
-		"name" : "Latte",
-		"cost" : "$2.85"
-	}, {
-		"name" : "Americano",
-		"cost" : "$3.85"
-	} ];
+app.controller('DrinkCtrl', function($scope, drinkService) {
+	drinkService.getDrinks(function (response){
+	console.log(response);
+	$scope.drinks = response;
+	});
 
 	$scope.buyDrink = function(drink) {
 		$scope.selection = drink;
@@ -49,23 +49,14 @@ app.controller('DrinkCtrl', function($scope) {
 
 app.controller('IngredientCtrl', function($scope,ingredientService) {
 	
-	var ingredients = ingredientService.getIngredients();
-	console.log(ingredients);
-	
-	$scope.inventory = [ {
-		"ingredient" : "Coffee",
-		"currentUnits" : 10
-	}, {
-		"ingredient" : "Sugar",
-		"currentUnits" : 10
-	}, {
-		"ingredient" : "Espresso",
-		"currentUnits" : 10
-	} ];
+	ingredientService.getIngredients(function(response){
+		console.log(response);
+		$scope.drinks = response;
+		});
 
-	$scope.restock = function(ingredient) {
-		$scope.selection = ingredient;
-		$scope.selection.currentUnits=ingredient.currentUnits;
-		console.log($scope.selection.currentUnits);
-	};
+		$scope.restock = function(ingredient) {
+			$scope.selection = ingredient;
+			$scope.selection.currentUnits=ingredient.currentUnits;
+			console.log($scope.selection.currentUnits);
+		};
 });
